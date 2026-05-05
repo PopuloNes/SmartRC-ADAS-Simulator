66cd2dd977bfce44aa672403983b7d75b8088aa0
PopuloNes <metrolast.late2033@gmail.com>
2026-05-05 15:40:12 +0200

    Format constants in ThermalSystem.h

--- include/ThermalSystem.h
+++ include/ThermalSystem.h
@@ -6,10 +6,10 @@
 class ThermalSystem {
 private:
     const float AMBIENT_TEMP = 25.0f;
-    const float WARNING_TEMP = 80.0f;   // Желтая зона (Предупреждение)
-    const float CRITICAL_TEMP = 120.0f; // Красная зона (Глушим мотор)
-    const float MAX_TEMP = 125.0f;      // Жесткий лимит ("Запрет на создание Солнца")
-    const float SAFE_TEMP = 60.0f;      // Температура остывания
+    const float WARNING_TEMP = 80.0f;  
+    const float CRITICAL_TEMP = 120.0f; 
+    const float MAX_TEMP = 125.0f;      
+    const float SAFE_TEMP = 60.0f;      
 
 public:
     void update(VehicleState& state);
